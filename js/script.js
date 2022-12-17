79f548aabaf91de8ee27b97b95eb6f1628a8154d
katebanban <101980471+katebanban@users.noreply.github.com>
2022-12-17 18:22:24 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//$('.slider__body').slick({
+//	infinity: true,
+//	autoplay: true
+//});
+
+const menuBtn = document.querySelector('.menu__btn');
+const menuList = document.querySelector('.menu__list');
+
+
+menuBtn.addEventListener('click', function () {
+	menuList.classList.toggle('menu__list--active');
+	menuBtn.classList.toggle('menu__btn--active');
+	document.querySelector('body').classList.toggle('no-scroll');
+});
+
+Fancybox.bind("[data-fancybox]", {
+	// Your options go here
+});
