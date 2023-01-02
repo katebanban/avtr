0bb4150916b166957d5b2d9b0e8092202bd99709
katebanban <101980471+katebanban@users.noreply.github.com>
2023-01-02 17:50:36 +0300

    added the "slider" block

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
-//$('.slider__body').slick({
-//	infinity: true,
-//	autoplay: true
-//});
+$('.design-slider').slick({
+	dots: false,
+	slidesToShow: 4,
+	variableWidth: true,
+	infinity: true,
+	prevArrow: '<img class="arrow arrow-left" src="img/design/icons/arrow-left.svg" alt="arrow left">',
+	nextArrow: '<img class="arrow arrow-right" src="img/design/icons/arrow-right.svg" alt="arrow right">',
+	//autoplay: true,
+});
 
 const menuBtn = document.querySelector('.menu__btn');
 const menuList = document.querySelector('.menu__list');
