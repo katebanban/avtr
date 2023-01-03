9aa803f122d5b65fddd3107fd9949071c5ee5b71
katebanban <101980471+katebanban@users.noreply.github.com>
2023-01-03 16:03:15 +0300

    finished the "design" section

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $('.design-slider').slick({
 	infinity: true,
 	prevArrow: '<img class="arrow arrow-left" src="img/design/icons/arrow-left.svg" alt="arrow left">',
 	nextArrow: '<img class="arrow arrow-right" src="img/design/icons/arrow-right.svg" alt="arrow right">',
-	//autoplay: true,
+	autoplay: true,
 });
 
 const menuBtn = document.querySelector('.menu__btn');
